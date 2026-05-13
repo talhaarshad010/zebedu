f08776f68aed467bf159c3788d8f25c38ccf507c
SoftAppsio.ca <122226517+talhaarshad010@users.noreply.github.com>
2026-05-14 00:39:37 +0500

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -13,6 +13,7 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  //dsdsd
   const [role, setRole] = useState<UserRole>(null);
   const [isFirstTime, setIsFirstTime] = useState(true);
 
